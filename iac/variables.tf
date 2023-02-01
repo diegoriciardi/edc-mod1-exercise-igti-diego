977a3c94d8f0cf1920e91210a0c8d8fe5f3ee6f6
diegoriciardi <diegoriciardi@gmail.com>
2023-01-31 23:30:06 -0300

    first steps with terraform

---
+++ iac/variables.tf
@@ -0,0 +1,11 @@
+variable "base_bucket_name" {
+  default = "datalake-dr-igti-ed-tf"
+}
+
+variable "ambiente" {
+  default = "producao"
+}
+
+variable "numero_conta" {
+  default = "195195321098"
+}
